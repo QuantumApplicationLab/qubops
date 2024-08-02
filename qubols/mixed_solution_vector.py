e990832b507eb0a4170c19355ccf0bae9c6ff0f9
Nicolas Renaud <nicolas.gm.renaud@gmail.com>
2024-08-02 13:41:13 +0200

    added mixed encoding solution

---
+++ qubols/mixed_solution_vector.py
@@ -0,0 +1,87 @@
+from sympy.matrices import Matrix
+import numpy as np
+from .encodings import RangedEfficientEncoding
+
+
+class MixedSolutionVector(object):
+
+    def __init__(self, solution_vectors):
+        """init the mixed solution vector
+
+        Args:
+            solution_vectors (List): A list of SolutionVector instances
+        """
+        self.solution_vectors = solution_vectors
+        self.nqbit = []
+        self.idx_start_data = self.get_indexes_data()
+        self.encoded_reals = self.create_encoding()
+
+    def create_encoding(self):
+        """Create the enconding for all the unknowns
+
+
+        Returns:
+            list[RealEncoded]:
+        """
+        encoded_reals = []
+
+        idx_vars = 0
+        for sol_vec in self.solution_vectors:
+            is_ranged_encoding = sol_vec.encoding == RangedEfficientEncoding
+            for i in range(sol_vec.size):
+                var_base_name = sol_vec.base_name + "_%03d" % (idx_vars + 1)
+                idx_vars += 1
+
+                if is_ranged_encoding:
+                    args = (
+                        sol_vec.nqbit,
+                        sol_vec.range[i],
+                        sol_vec.offset[i],
+                        var_base_name,
+                    )
+                else:
+                    args = (sol_vec.nqbit, var_base_name)
+                encoded_reals.append(sol_vec.encoding(*args))
+                self.nqbit.append(sol_vec.nqbit)
+        return encoded_reals
+
+    def get_indexes_data(self):
+        """Get the indices of the start/end of the data for each encoding
+
+        Returns:
+            np.array: lis of start/end index
+        """
+        idx_start_data = [0]
+        for sv in self.solution_vectors:
+            idx_start_data.append(sv.nqbit * sv.size)
+        idx_start_data[-1] += 1
+        return np.cumsum(idx_start_data)
+
+    def create_polynom_vector(self):
+        """Create the list of polynom epxressions
+
+        Returns:
+            sympy.Matrix: matrix of polynomial expressions
+        """
+        pl = []
+        for real in self.encoded_reals:
+            pl.append(real.create_polynom())
+        return Matrix(pl)
+
+    def decode_solution(self, data):
+        """Decode the solution
+
+        Args:
+            data (list): data from the annealer
+
+        Returns:
+            list: decoded numbers
+        """
+        sol = []
+        for iv, sv in enumerate(self.solution_vectors):
+            idx_start = self.idx_start_data[iv]
+            idx_end = self.idx_start_data[iv + 1]
+            sv_data = data[idx_start:idx_end]
+            sol.append(list(sv.decode_solution(sv_data)))
+            idx_start += sv.size
+        return sol
---
+++ qubols/qubo_poly_mixed_variables.py
@@ -0,0 +1,219 @@
+from sympy.matrices import Matrix
+import numpy as np
+from typing import Optional, Union, Dict
+import neal
+import dimod
+from .qubo_poly import QUBO_POLY
+from .mixed_solution_vector import MixedSolutionVector
+
+
+class QUBO_POLY_MIXED(QUBO_POLY):
+
+    def __init__(
+        self,
+        mixed_solution_vectors: MixedSolutionVector,
+        options: Optional[Union[Dict, None]] = None,
+    ):
+        """Polynomial of degree 2 Solver using QUBO
+
+        Solve the following equation
+
+        ..math:
+            P_0 + P_1 x + P_2 x \otimes x = 0
+
+        Args:
+            options: dictionary of options for solving the linear system
+        """
+
+        default_solve_options = {
+            "sampler": neal.SimulatedAnnealingSampler(),
+            "num_reads": 100,
+            "verbose": False,
+        }
+        self.options = self._validate_solve_options(options, default_solve_options)
+        self.sampler = self.options.pop("sampler")
+        self.mixed_solution_vectors = mixed_solution_vectors
+
+    def create_bqm(self, matrices):
+        """Create the bqm from the matrices
+
+        Args:
+            matrices (tuple): matrix of the system
+
+        Returns:
+            dimod.bqm: binary quadratic model
+        """
+
+        self.matrices = matrices
+        self.num_variables = self.matrices[1].shape[1]
+
+        self.x = self.mixed_solution_vectors.create_polynom_vector()
+        self.extract_all_variables()
+
+        return self.create_qubo_matrix(self.x)
+
+    def sample_bqm(self, bqm):
+        """Sample the bqm"""
+
+        return self.sampler.sample(bqm, num_reads=self.options["num_reads"])
+
+    def decode_solution(self, solution):
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
+        idx, vars, data = self.extract_data(solution)
+        return self.mixed_solution_vectors.decode_solution(data)
+
+    def solve(self, matrices):
+        """Solve the linear system
+
+        Args:
+            sampler (_type_, optional): _description_. Defaults to neal.SimulatedAnnealingSampler().
+            encoding (_type_, optional): _description_. Defaults to RealUnitQbitEncoding.
+            nqbit (int, optional): _description_. Defaults to 10.
+
+        Returns:
+            _type_: _description_
+        """
+
+        # create the bqm
+        self.qubo_dict = self.create_bqm(matrices)
+
+        # sample the bqm
+        self.sampleset = self.sample_bqm(self.qubo_dict)
+        self.lowest_sol = self.sampleset.lowest()
+
+        # sample the systen and return the solution
+        return self.decode_lowest_solution(self.lowest_sol)
+
+    def extract_all_variables(self):
+        """Extracs all the variable names and expressions"""
+        self.all_vars = []
+        self.all_expr = []
+        for var in self.x:
+            expr = [(str(k), v) for k, v in var.as_coefficients_dict().items()]
+            self.all_expr.append(expr)
+            self.all_vars += [str(k) for k in var.as_coefficients_dict().keys()]
+
+    def create_polynom(self, x: np.ndarray):
+        """Creates the polynom from the matrices
+
+        Args:
+            x (_type_): _description_
+        """
+        self.num_equations = self.matrices[1].shape[0]
+        polynom = Matrix([0] * self.num_equations)
+
+        for imat, matrix in enumerate(self.matrices):
+
+            for idx, val in zip(matrix.coords.T, matrix.data):
+                if imat == 0:
+                    polynom[idx[0]] += val
+                else:
+                    polynom[idx[0]] += val * x[idx[1:]].prod()
+        return polynom
+
+    def create_qubo_matrix(self, x, strength=100, prec=None):
+        """Create the QUBO dictionary requried by dwave solvers
+        to solve the polynomial equation P0 + P1 x + P2 x x = 0
+
+
+        Args:
+            Anp (np.array): matrix of the linear system
+            bnp (np.array): righ hand side of the linear system
+            x (sympy.Matrix): unknown
+
+        Returns:
+            _type_: _description_
+        """
+
+        polynom = self.create_polynom(np.array(x))
+
+        polynom = polynom.T @ polynom
+
+        polynom = polynom[0]
+        polynom = polynom.expand()
+        polynom = polynom.as_ordered_terms()
+        polynom = self.create_poly_dict(polynom, prec=prec)
+        bqm = dimod.make_quadratic(polynom, strength=strength, vartype=dimod.BINARY)
+
+        return bqm
+
+    @staticmethod
+    def create_poly_dict(polynom, prec=None):
+        """Creates a dict from the sympy polynom
+
+        Args:
+            polynom (_type_): _description_
+
+        Returns:
+            Dict: _description_
+        """
+        out = dict()
+
+        for term in polynom:
+            m = term.args
+            if len(m) == 0:
+                continue
+
+            if len(m) == 2:
+                varname = str(m[1])
+                tmp = varname.split("**")
+                if len(tmp) == 1:
+                    exponent = 1
+                else:
+                    varname, exponent = tmp
+                    exponent = int(exponent)
+                key = (varname,) * exponent
+
+            elif len(m) > 2:
+                key = tuple()
+                for mi in m[1:]:
+                    mi = str(mi)
+                    tmp = mi.split("**")
+                    if len(tmp) == 1:
+                        key += (tmp[0],)
+                    if len(tmp) == 2:
+                        varname = tmp[0]
+                        exp = int(tmp[1])
+                        key += (varname,) * exp
+
+            if key not in out:
+                out[key] = 0.0
+
+            out[key] += m[0]
+
+        if prec is None:
+            return out
+
+        elif prec is not None:
+            nremoved = 0
+            out_cpy = dict()
+            for k, v in out.items():
+                if np.abs(v) > prec:
+                    out_cpy[k] = v
+                else:
+                    nremoved += 1
+            print("Removed %d elements" % nremoved)
+            return out_cpy
+
+    def extract_data(self, sol):
+        """Extracts the data from the solution
+
+        Args:
+            sol (_type_): _description_
+        """
+        # extract the data of the original variables
+        idx, vars = [], []
+        for ix, s in enumerate(sol.variables):
+            # if s.startswith("slack"):
+            #     continue
+            # if len(s.split("*")) == 1:
+            if s in self.all_vars:
+                idx.append(ix)
+                vars.append(s)
+
+        data = sol.record[0][0]
+        return idx, vars, data[idx]
